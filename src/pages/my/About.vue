890b88a82c0f0ada44f99f3c55fa02a781b18109
qinhua <352484005@qq.com>
2017-08-07 23:35:40 +0800

    :laughing:默默提交一下今天的修改

---
+++ src/pages/my/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="aboutus">
+      关于连你
+  </div>
+</template>
+
+<script>
+  /* eslint-disable no-unused-vars */
+  var me
+  var vm
+  import {Grid, GridItem, Group, Cell} from 'vux'
+  export default {
+    name: 'aboutus',
+    data () {
+      return {
+        address: null
+      }
+    },
+    components: {Grid, GridItem, Group, Cell},
+    beforeMount () {
+      me = window.me
+    },
+    mounted () {
+      // me.attachClick()
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='less'>
+  @import '../../../static/css/tools.less';
+  .my-address{
+    padding-bottom: 60px;
+    .add-address{
+      .borBox;
+      padding:30/@rem;
+      .center;
+      .bf;
+      /*background:#5c70ff url(../../../static/img/bg_av.jpg);
+      .bg100;*/
+      span{
+        .fz(26);
+        .c3;
+      }
+    }
+    .address-list{
+      .weui-cells{
+        margin-top:10/@rem;
+        padding: 0;
+      }
+      .weui-cell{
+        padding:20/@rem 30/@rem!important;
+        .rfz(14)!important;
+      }
+    }
+  }
+
+</style>
