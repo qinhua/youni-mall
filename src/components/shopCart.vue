59282cb462e57f3425fdc6c91dd4ad2d8e1da8a8
qinhua <352484005@qq.com>
2017-08-21 11:43:46 +0800

    no message

---
+++ src/components/shopCart.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+  let me
+  let vm
+  export default {
+    name: 'shopCart',
+    data () {
+      return {
+        count: 0
+      }
+    },
+    mounted () {
+      me = window.me
+      vm = this
+    },
+    methods () {
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
